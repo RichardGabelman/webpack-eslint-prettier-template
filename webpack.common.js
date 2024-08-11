32a5b8fdbf2ae99c6e5ee46891a160f1b9ca11c0
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-08-11 14:17:38 -0700

    Add html-loader

--- webpack.common.js
+++ webpack.common.js
@@ -15,6 +15,11 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      // Image files referenced in HTML template
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
