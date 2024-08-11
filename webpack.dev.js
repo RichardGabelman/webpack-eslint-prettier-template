3fc3cb8af8d234a6a7b4c82714917954df00d506
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-08-11 14:22:31 -0700

    Fix file path name

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
-    watchFiles: ["src/index.html"],
+    watchFiles: ["./src/index.html"],
   },
   module: {
     rules: [
